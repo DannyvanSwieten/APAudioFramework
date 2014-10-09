5e76d572b79ebb924bec69f46afe6556c27ca81a
Danny van Swieten <Dannyvanswieten@gmail.com>
2014-10-09 11:56:57 +0200

    Added class for processing sensor data

---
+++ iOSSensorWrappers/APAudioSensorProcessor.cpp
@@ -0,0 +1,9 @@
+//
+//  APAudioSensorProcessor.cpp
+//  FuChair
+//
+//  Created by Danny van Swieten on 10/9/14.
+//
+//
+
+#include "APAudioSensorProcessor.h"
---
+++ iOSSensorWrappers/APAudioSensorProcessor.h
@@ -0,0 +1,23 @@
+//
+//  APAudioSensorProcessor.h
+//  FuChair
+//
+//  Created by Danny van Swieten on 10/9/14.
+//
+//
+
+#ifndef __FuChair__APAudioSensorProcessor__
+#define __FuChair__APAudioSensorProcessor__
+
+#include "Acceleration.h"
+
+class APAudioSensorProcessor
+{
+public:
+    
+private:
+    
+    APAAcceleration 
+};
+
+#endif /* defined(__FuChair__APAudioSensorProcessor__) */
