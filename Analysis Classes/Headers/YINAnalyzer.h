14426e4ee99e7b206f08ba53b39bcc78d16a60a2
Danny van Swieten <Dannyvanswieten@gmail.com>
2014-11-18 10:42:34 +0100

    pitch analysis class

---
+++ Analysis Classes/Headers/YINAnalyzer.h
@@ -0,0 +1,37 @@
+//
+//  FrequencyAnalyzer.h
+//  APAAnalyzer
+//
+//  Created by Danny van Swieten on 10/2/14.
+//
+//
+
+#ifndef __APAAnalyzer__FrequencyAnalyzer__
+#define __APAAnalyzer__FrequencyAnalyzer__
+#include "YIN.h"
+#include "APAudioFileManager2.h"
+#include "AudioAnalyzer.h"
+
+class YINAnalyzer: AudioAnalyzer
+{
+public:
+    
+    YINAnalyzer();
+    void readAndAnalyse(const float* input, long numberOfSamples) override final;
+    void calculateAmplitudes()override final;
+    void calculatePhases()override final;
+    void calculateInstantFrequencies()override final;
+    void calculatePitch() override final;
+    std::vector<float> getResult(){return _result;};
+    void init(int N);
+    
+private:
+    
+    YIN yin;
+    float _prevSample = 0;
+    int _downsample = 1;
+    int _N;
+    std::vector<float> _result;
+};
+
+#endif /* defined(__APAAnalyzer__FrequencyAnalyzer__) */
