8a218c92597e419985608e31ecfdde090bb17927
Danny van Swieten <Dannyvanswieten@gmail.com>
2014-12-30 12:03:29 +0100

    deleted unused cpp file

--- Utility files/Parameter.h
+++ Utility files/Parameter.h
@@ -9,55 +9,47 @@
 #ifndef __APAudioEngine__APAudioParameter__
 #define __APAudioEngine__APAudioParameter__
 
-#include "AudioModule.h"
-
-namespace APAudio
+template<typename T>
+class Parameter
 {
-    namespace Audio
+public:
+    Parameter(){};
+    Parameter(T min,
+              T max,
+              T start,
+              std::string identification)
     {
-        template<typename T>
-        class Parameter
-        {
-        public:
-            Parameter(){};
-            Parameter(T min,
-                      T max,
-                      T start,
-                      std::string identification)
-            {
-                _minValue = min;
-                _maxValue = max;
-                _currentValue = start;
-                
-                _ID = identification;
-            }
-            
-            void setMinValue(T value){_minValue = value;};
-            void setMaxValue(T value){_maxValue = value;};
-            void setValue(T value)
-            {
-                if(value > _maxValue) _currentValue = _maxValue;
-                else if(value < _minValue) _currentValue = _minValue;
-                else _currentValue = value;
-            };
-            
-            Parameter& operator= (const T& value){setValue(value); return *this;};
-            Parameter& operator++ (){setValue(_currentValue++); return *this;};
-            
-            inline T getMinValue(){return _minValue;};
-            inline T getMaxValue(){return _maxValue;};
-            inline T getValue(){return _currentValue;};
-            inline std::string  getID(){return _ID;};
-            
-        private:
-            
-            T _minValue;
-            T _maxValue;
-            T _currentValue;
-            
-            std::string _ID;
-        };
+        _minValue = min;
+        _maxValue = max;
+        _currentValue = start;
+        
+        _ID = identification;
     }
-}
+    
+    void setMinValue(T value){_minValue = value;};
+    void setMaxValue(T value){_maxValue = value;};
+    void setValue(T value)
+    {
+        if(value > _maxValue) _currentValue = _maxValue;
+        else if(value < _minValue) _currentValue = _minValue;
+        else _currentValue = value;
+    };
+    
+    Parameter& operator= (const T& value){setValue(value); return *this;};
+    Parameter& operator++ (){setValue(_currentValue++); return *this;};
+    
+    inline T getMinValue(){return _minValue;};
+    inline T getMaxValue(){return _maxValue;};
+    inline T getValue(){return _currentValue;};
+    inline std::string  getID(){return _ID;};
+    
+private:
+    
+    T _minValue;
+    T _maxValue;
+    T _currentValue;
+    
+    std::string _ID;
+};
 
 #endif /* defined(__APAudioEngine__APAudioParameter__) */
